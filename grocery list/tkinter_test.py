d9a982e624eafe06a29abfd9cf6a7ec4f8a40b7e
Chaitra98787 <116797589+Chaitra98787@users.noreply.github.com>
2025-02-20 22:16:09 +0530

    tkinter_test.py

--- grocery list/tkinter_test.py
+++ grocery list/tkinter_test.py
@@ -1,15 +1,12 @@
 import tkinter as tk
 
-# Create main window
 root = tk.Tk()
 root.title("Grocery Store Management")
 root.geometry("400x300")  # Set window size
 
-# Create a label
 label = tk.Label(root, text="Welcome to Grocery Store", font=("Arial", 14))
 label.pack(pady=20)  # Add space around the label
 
-# Add a button
 btn = tk.Button(root, text="Click Me")
 
 
